4d8dac95ccab4bf1db027c5eff9b2fa8c50fb2fc
a.chubutkin <a.chubutkin@gmail.com>
2017-04-11 13:00:15 +0600

    Add push plugin.

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -44,6 +44,22 @@ module.exports = [
         "id": "cordova-plugin-network-information.NetworkInfoProxy",
         "pluginId": "cordova-plugin-network-information",
         "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
+        "id": "phonegap-plugin-push.BrowserPush",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -53,7 +69,8 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "4.0.3-dev",
     "cordova-plugin-statusbar": "1.0.1",
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-network-information": "1.3.2"
+    "cordova-plugin-network-information": "1.3.2",
+    "phonegap-plugin-push": "1.10.1"
 }
 // BOTTOM OF METADATA
 });
